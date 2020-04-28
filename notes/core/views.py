94e747ad746a21ac10fa8aa2038c41d68808136e
MFvanRossum <matissev@hotmail.com>
2020-04-28 13:04:24 -0400

    Remove note list page in favor of sidebar. Homepage defaults to new note

--- notes/core/views.py
+++ notes/core/views.py
@@ -4,10 +4,6 @@ from django.http import HttpResponse
 from .models import Note
 from .forms import NoteForm
 
-def notes_list(request):
-    notes = Note.objects.all()
-    return render(request, 'core/notes_list.html', {'notes': notes})
-
 def notes_detail(request, pk):
     note = Note.objects.get(pk=pk)
     notes = Note.objects.all()
